90b66e9d6a3ed1fb064ecd734fd978c6a5bb0aff
sayonara213 <maxsai567@gmail.com>
2023-08-30 15:31:27 +0300

    FE: fixed theme persistence

---
+++ src/app/app/wishlist/layout.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { cookies } from 'next/headers';
+
+import styles from './wishlist-layout.module.scss';
+
+import { toNormalCase } from '@/utils/text';
+
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+
+const WishlistLayout = async ({ children }: { children: React.ReactNode }) => {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const username = await supabase.from('profiles').select('user_name').eq('id', user!.id);
+
+  return (
+    <div className={styles.container}>
+      {toNormalCase(username.data![0].user_name)}
+      {children}
+    </div>
+  );
+};
+
+export default WishlistLayout;
