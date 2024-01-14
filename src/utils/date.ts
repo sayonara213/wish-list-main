c9334123f4eedc25e57e2fc778f67c1b51a438b3
sayonara213 <maxsai567@gmail.com>
2024-01-14 18:47:23 +0200

    FE: date timezone revert

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,18 +1,5 @@
 import { formatDistanceToNow } from 'date-fns';
 
-const convertUTCDateToLocalDate = (date: string) => {
-  const dateObj = new Date(date);
-
-  var newDate = new Date(dateObj.getTime() + dateObj.getTimezoneOffset() * 60 * 1000);
-
-  const offset = dateObj.getTimezoneOffset() / 60;
-  const hours = dateObj.getHours();
-
-  newDate.setHours(hours - offset);
-
-  return newDate;
-};
-
 export const formatDateToNow = (date: string) => {
   const dateObj = new Date(date);
 
